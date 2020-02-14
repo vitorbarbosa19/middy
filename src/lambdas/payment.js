5cb091e9c3cad6ebd2d9d80598e8997bcfaf1db7
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-02-13 18:29:42 -0800

    add error modules

--- src/lambdas/payment.js
+++ src/lambdas/payment.js
@@ -1,6 +1,8 @@
 const middy = require('@middy/core')
 const httpJsonBodyParser = require('@middy/http-json-body-parser')
 const httpUrlencodeBodyParser = require('@middy/http-urlencode-body-parser')
+const httpErrorHandler = require('@middy/http-error-handler')
+const createError = require('http-errors')
 
 const payment = async event => {
 	try {
@@ -8,20 +10,17 @@ const payment = async event => {
 		const { number, expiry, cvc, name, amount } = event.body
 		console.log(number)
 		console.log(expiry)
+		throw new createError(401, 'Unauthenticated')
 		return {
 			statusCode: 200,
 			body: 'Ok'
 		}
-	} catch (error) {
-		return {
-			statusCode: 500,
-			body: 'error'
-		}
-	}
+	} catch (error) { throw error }
 }
 
 const handler = middy(payment)
 	.use(httpJsonBodyParser())
 	.use(httpUrlencodeBodyParser())
+	.use(httpErrorHandler())
 
 module.exports = { handler }
